b6d1ea3f046205f4255e8e542742c9a83d806dc8
Aleksandr Belyaev <belyaevalexande@mail.ru>
2020-10-18 14:41:49 +0300

    welcome phrases added

--- bot.py
+++ bot.py
@@ -5,10 +5,14 @@ TOKEN = os.environ["TOKEN"]
 
 bot = telebot.TeleBot(TOKEN)
 
+bot.send_message("Привет! Я учу людей испанскому!")
+bot.send_message("Наши курсы состоят из 10 уроков, которые приходят каждый день")
+bot.send_message("https://m.youtube.com/watch?v=Cn-q-04L1ZE")
 
-@bot.message_handler(commands=['start', 'help'])
+
+@bot.message_handler(commands=['start', 'help'])  # отбивка функции в две строчки
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я учу людей испанскому!")
+    bot.reply_to(message, "Поехали!")
 
 
 bot.polling()
